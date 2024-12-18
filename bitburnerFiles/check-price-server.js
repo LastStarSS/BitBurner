4eda014145a6b97b07db8c5776ca643174fd1eeb
Khang Tran <doneekhang@gmail.com>
2024-12-17 21:47:24 -0500

    Deleting script duplicates

--- bitburnerFiles/check-price-server.js
+++
@@ -1,4 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-  ns.tprint(ns.getPurchasedServerCost(ns.args[0]))
-}
--- bitburnerFiles/get-max-money.js
+++
@@ -1,4 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-  ns.tprint("The server's max money (M) is: " + ns.getServerMaxMoney(ns.args[0])/1000000)
-}
